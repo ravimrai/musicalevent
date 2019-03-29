25b2d3c2635c6d2cec85455423c70a6a9a808c82
ravimrai <ravimadhav1707@gmail.com>
2019-03-30 00:54:12 +0530

    Updates

---
+++ precache-manifest.6f8b5418c99fd7bc3669c09f5d1f5d93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/musicalevent/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/musicalevent/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/musicalevent/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "7443e83b2ae82739edda",
+    "url": "/musicalevent/static/js/runtime~main.7443e83b.js"
+  },
+  {
+    "revision": "a3b0967c1ed0a536d88c",
+    "url": "/musicalevent/static/js/main.a3b0967c.chunk.js"
+  },
+  {
+    "revision": "6051aee7b3cdd634ad8e",
+    "url": "/musicalevent/static/js/1.6051aee7.chunk.js"
+  },
+  {
+    "revision": "a3b0967c1ed0a536d88c",
+    "url": "/musicalevent/static/css/main.9795f52e.chunk.css"
+  },
+  {
+    "revision": "bba088c307c19607befe97486a5b8544",
+    "url": "/musicalevent/index.html"
+  }
+];
